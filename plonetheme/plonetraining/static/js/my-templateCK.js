924d9e162c7cc00e03f14c44c9f0a51dc3b5ad38
Stéphane Moussiaux <smoussiaux@cirb.irisnet.be>
2013-09-03 12:07:59 +0200

    add template CKeditor

--- plonetheme/plonetraining/static/js/my-templateCK.js
+++ plonetheme/plonetraining/static/js/my-templateCK.js
@@ -18,7 +18,6 @@ CKEDITOR.addTemplates( 'default',
 			{
 				title: 'Template 3col',
 				html:
-					'<h3>Template 3col</h3>' +
 					'<div class="contentcol">' +
                     '<div class="towcol1 float3"><h3>title col1</h3>col1</div>' +
                     '<div class="towcol2 float3"><h3>title col2</h3>col2</div>' +
@@ -30,42 +29,11 @@ CKEDITOR.addTemplates( 'default',
 			{
 				title: 'Template 2col',
 				html:
-				    '<h2> Template 2col </h2>' +
                     '<div class="contentcol">' +
 				    '<div class="towcol1 float"><h3>title col1</h3>col1</div>' +
 				    '<div class="towcol2 float"><h3>title col2</h3>col2</div>' +
                     '<div class="clearboth"></div>' +
                     '</div>'
-			},
-            {
-                title:'Accordione2',
-                html:
-                    '<div id="accordion2" class="accordion">' +
-                    '   <div class="accordion-group">' +
-                    '      <div class="accordion-heading">' +
-                    '       <a href="#collapseOne" data-parent="#accordion2" data-toggle="collapse" class="accordion-toggle collapsed">' +
-                    '            Collapsible Group Item #1' +
-                    '       </a>' +
-                    '        </div>' +
-                    '        <div class="accordion-body collapse" id="collapseOne" style="height: 0px;">' +
-                    '            <div class="accordion-inner">' +
-                    '                 Anim pariatur cliche...' +
-                    '            </div>' +
-                    '         </div>' +
-                    '      </div>' +
-                    '   <div class="accordion-group">' +
-                    '        <div class="accordion-heading">' +
-                    '             <a href="#collapseTwo" data-parent="#accordion2" data-toggle="collapse" class="accordion-toggle">' +
-                    '                  Collapsible Group Item #2' +
-                    '               </a>' +
-                    '         </div>' +
-                    '          <div class="accordion-body collapse" id="collapseTwo">' +
-                    '             <div class="accordion-inner">' +
-                    '                   Anim pariatur cliche...' +
-                    '             </div>' +
-                    '           </div>' +
-                    '   </div>' +
-                    '</div>'
-            }
+			}
 		]
 });
